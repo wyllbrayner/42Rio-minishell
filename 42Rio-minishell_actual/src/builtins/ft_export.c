059e0fd514e7c13fa8f74b7100f3c11676d0d2ed
woliveir <wyllbrayner@yahoo.com.br>
2023-02-03 22:19:28 -0300

    20230203 | 42RIO export 2

--- 42Rio-minishell_actual/src/builtins/ft_export.c
+++ 42Rio-minishell_actual/src/builtins/ft_export.c
@@ -23,70 +23,71 @@ void ft_builtin_export(t_minishell *sh)
 
     start = ft_strchr_i(sh->head->cmd[0], ' ');
     end = ft_strchr_i(sh->head->cmd[0], '=');
-    sh->tmp1 = ft_substr(sh->head->cmd[0], (start + 1), end - start);
-    printf("Dentro da export | start: %ld | end: %ld | sub_str: %s\n", start, end, sh->tmp1);
-
-    i = 0;
-    status = 0;
-    while (sh->env[i])
+    if (end > start)
     {
-        if (ft_strncmp(sh->env[i], sh->tmp1, (end - start)) == 0)
+        sh->tmp1 = ft_substr(sh->head->cmd[0], (start + 1), end - start);
+        printf("Dentro da export | start: %ld | end: %ld | sub_str: %s\n", start, end, sh->tmp1);
+        i = 0;
+        status = FALSE;
+        while (sh->env[i])
         {
-            status = 1;
-            break ;
+            if (ft_strncmp(sh->env[i], sh->tmp1, (end - start)) == 0)
+            {
+                status = TRUE;
+                break ;
+            }
+            i++;
         }
-        i++;
-    }
-    if (status == 1)
-    {
-        printf("    Achei a variável de ambiente: %s em env[%ld]: %s\n", sh->tmp1, i, sh->env[i]);
-        ft_free_minishell_single_aux(sh->env[i]);
-        sh->env[i] = NULL;
-        sh->env[i] = ft_substr(sh->head->cmd[0], (start + 1), (ft_strlen(sh->head->cmd[0]) - end + 4));
+//      if (status == 1)
+        if (status)
+        {
+            printf("    Achei a variável de ambiente: %s em env[%ld]: %s\n", sh->tmp1, i, sh->env[i]);
+            ft_free_minishell_single_aux(sh->env[i]);
+            sh->env[i] = NULL;
+            sh->env[i] = ft_substr(sh->head->cmd[0], (start + 1), (ft_strlen(sh->head->cmd[0]) - end + 4));
+        }
+        else
+        {
+            printf("Não achei a variável de ambiente: %s em env[%ld]: %s\n", sh->tmp1, i, sh->env[i]);
+            printf("Total de variáveis de ambiente: %ld\n", i);
+            i++;
+            sh->tmp3 = (char **)malloc(sizeof(char *) * (++i));
+            if (!sh->tmp3)
+            {
+                ft_free_minishell_single_aux(sh->tmp1);
+                sh->ret = -3;
+                return ;
+            }
+            sh->tmp3[--i] = NULL;
+            sh->tmp3[--i] = ft_substr(sh->head->cmd[0], (start + 1), (ft_strlen(sh->head->cmd[0]) - end + 4));
+            while(--i >= 0)
+            {
+                sh->tmp3[i] = sh->env[i];
+                sh->env[i] = NULL;
+            }
+            ft_free_minishell_double_aux(sh->env);
+            sh->env = NULL;
+            sh->env = sh->tmp3;
+        }
+        ft_free_minishell_single_aux(sh->tmp1);
+        sh->tmp1 = NULL;
+        sh->tmp3 = NULL;
+        /*
+        i = 0;
+        while (sh->env[i])
+        {
+            printf("sh->env[%ld]: %s\n", i, sh->env[i]);
+            i++;
+        }
+        */
     }
     else
     {
-        printf("Não achei a variável de ambiente: %s em env[%ld]: %s\n", sh->tmp1, i, sh->env[i]);
-/*
         i = 0;
         while (sh->env[i])
+        {
+            printf("declare -x %s\n", sh->env[i]);
             i++;
-*/
-        printf("Total de variáveis de ambiente: %ld\n", i);
-        i++;
-        sh->tmp3 = (char **)malloc(sizeof(char *) * (++i));
-        if (!sh->tmp3)
-        {
-            ft_free_minishell_single_aux(sh->tmp1);
-            sh->ret = -3;
-            return ;
         }
-        sh->tmp3[--i] = NULL;
-        sh->tmp3[--i] = ft_substr(sh->head->cmd[0], (start + 1), (ft_strlen(sh->head->cmd[0]) - end + 4));
-        while(--i >= 0)
-        {
-            sh->tmp3[i] = sh->env[i];
-            sh->env[i] = NULL;
-        }
-        ft_free_minishell_double_aux(sh->env);
-        sh->env = NULL;
-        sh->env = sh->tmp3;
     }
-    ft_free_minishell_single_aux(sh->tmp1);
-    sh->tmp1 = NULL;
-    sh->tmp3 = NULL;
-    i = 0;
-    while (sh->env[i])
-    {
-        printf("sh->env[%ld]: %s\n", i, sh->env[i]);
-        i++;
-    }
-/*
-    i = 0;
-    while (sh->tmp3[i])
-    {
-        printf("sh->tmp3[%ld]: %s\n", i, sh->tmp3[i]);
-        i++;
-    }
-*/
 }
